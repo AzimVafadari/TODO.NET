e3c4ec734aae488f0d54804e89ff65ad7854f6f5
AV <azimvafadari@gmail.com>
2024-07-30 10:15:22 +0330

    create User model

---
+++ TODO/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TODO.Models;
+
+public class User
+{
+    [Key]
+    public int UserId { set; get; }
+    public string Username { set; get; } = string.Empty;
+    public string Password { set; get; } = string.Empty;
+}
