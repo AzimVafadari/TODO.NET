46f9b146aa9a71bbd2d924821685ce12947b0a5f
AV <azimvafadari@gmail.com>
2024-07-30 10:56:31 +0330

    add verification to User and Todo models

--- TODO/Models/Todo.cs
+++ TODO/Models/Todo.cs
@@ -6,8 +6,11 @@ namespace TODO.Models;
 public class Todo
 {
     [Key]
-    public int TodoId { set; get; }
+    public int TodoId { get; }
     public string Status { set; get; } = string.Empty;
+    [MinLength(4), MaxLength(15), Required]
+    public string Title { set; get; } = string.Empty;
+    [MinLength(4), MaxLength(30), Required]
     public string Description { set; get; } = string.Empty;
     public bool IsDeleted { set; get; }
     
--- TODO/Models/User.cs
+++ TODO/Models/User.cs
@@ -5,9 +5,11 @@ namespace TODO.Models;
 public class User
 {
     [Key]
-    public int UserId { set; get; }
+    public int UserId { get; }
+    [MinLength(4), MaxLength(10), Required]
     public string Username { set; get; } = string.Empty;
+    [MinLength(8), MaxLength(16), Required]
     public string Password { set; get; } = string.Empty;
     // Navigation property
-    public virtual ICollection<Todo> Todos { get; set; } = new List<Todo>();
+    public virtual ICollection<Todo> Todos { get; } = new List<Todo>();
 }
