6f3970946bf38239c0f298ccb9233b6f5b0e70df
AV <azimvafadari@gmail.com>
2024-07-30 10:33:17 +0330

    create Todo model

---
+++ TODO/Models/Todo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TODO.Models;
+
+public class Todo
+{
+    [Key]
+    public int TodoId { set; get; }
+    public string Status { set; get; } = string.Empty;
+    public string Description { set; get; } = string.Empty;
+    public bool IsDeleted { set; get; }
+    
+    // Foreign key for User
+    public int UserId { set; get; }
+
+    // Navigation property
+    [ForeignKey("UserId")]
+    public User User { set; get; }
+}
