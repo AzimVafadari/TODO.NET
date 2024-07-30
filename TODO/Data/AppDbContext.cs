f4cfdc09f1d50a3fa7eeb2d792f621bf923ec1bd
AV <azimvafadari@gmail.com>
2024-07-30 10:49:15 +0330

    add AppDbContext.cs

---
+++ TODO/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TODO.Models;
+
+namespace TODO.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { set; get; }
+        public DbSet<Todo> Todos { set; get; }
+    }
+}
--- TODO/obj/Debug/net8.0/TODO.AssemblyInfo.cs
+++ TODO/obj/Debug/net8.0/TODO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TODO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb6c38f9f19870e830b47e3a8e38050855ea713")]
 [assembly: System.Reflection.AssemblyProductAttribute("TODO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TODO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
