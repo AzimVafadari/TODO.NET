18cad9fd30c5937d6a340c0ce0ff7cc8c8d25e29
AV <azimvafadari@gmail.com>
2024-07-31 13:16:25 +0330

    adding UserAlreadyExistsException.cs

---
+++ TODO/Business/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,14 @@
+﻿namespace TODO.Business.Exceptions;
+
+public class UserAlreadyExistsException : Exception
+{
+    // Constructor that takes a message parameter
+    public UserAlreadyExistsException(string message) : base(message) 
+    {
+    }
+
+    // Constructor that takes a message and an inner exception
+    public UserAlreadyExistsException(string message, Exception innerException) : base(message, innerException) 
+    {
+    }
+}
