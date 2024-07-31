e5a13bc802f6f4de9637bec6a8823638d0943cb2
AV <azimvafadari@gmail.com>
2024-07-31 13:43:38 +0330

    adding UserNotFoundException.cs

---
+++ TODO/Business/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace TODO.Business.Exceptions;
+
+public class UserNotFoundException : Exception
+{
+    // Constructor that takes a message parameter
+    public UserNotFoundException(string message) : base(message) 
+    {
+    }
+    
+    public UserNotFoundException() 
+    {
+    }
+
+    // Constructor that takes a message and an inner exception
+    public UserNotFoundException(string message, Exception innerException) : base(message, innerException) 
+    {
+    }
+}
