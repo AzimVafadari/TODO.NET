b55d76f66b080f13c430de2d5aa31609f53c3ad8
AV <azimvafadari@gmail.com>
2024-08-01 13:48:17 +0330

    adding some exception class for todo

---
+++ TODO/Business/Exceptions/TodoAlreadyDeletedException.cs
@@ -0,0 +1,18 @@
+﻿namespace TODO.Business.Exceptions;
+
+public class TodoAlreadyDeletedException : Exception
+{
+    // Constructor that takes a message parameter
+    public TodoAlreadyDeletedException(string message) : base(message) 
+    {
+    }
+    
+    public TodoAlreadyDeletedException() 
+    {
+    }
+
+    // Constructor that takes a message and an inner exception
+    public TodoAlreadyDeletedException(string message, Exception innerException) : base(message, innerException) 
+    {
+    }
+}
---
+++ TODO/Business/Exceptions/TodoNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace TODO.Business.Exceptions;
+
+public class TodoNotFoundException : Exception
+{
+    // Constructor that takes a message parameter
+    public TodoNotFoundException(string message) : base(message) 
+    {
+    }
+    
+    public TodoNotFoundException() 
+    {
+    }
+
+    // Constructor that takes a message and an inner exception
+    public TodoNotFoundException(string message, Exception innerException) : base(message, innerException) 
+    {
+    }
+}
