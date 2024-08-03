37002c28d66735305d24b5096509f1caab4c0555
AV <azimvafadari@gmail.com>
2024-08-03 10:48:46 +0330

    Status.cs model is created

---
+++ TODO/Models/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace TODO.Models;
+
+public class Status
+{
+    public int StatusId { get; }
+
+    public string StatusName { get; set; } = string.Empty;
+}
--- TODO/obj/Debug/net8.0/TODO.AssemblyInfo.cs
+++ TODO/obj/Debug/net8.0/TODO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TODO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6491299265c7e2a81b564038c38819f0abf6d8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+336aab08c6ccf3d236239392de1c03cb8b55419b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TODO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TODO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
